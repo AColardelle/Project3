c1094544dcd36de457ff983c389dcc516b167328
AColardelle <67693923+AColardelle@users.noreply.github.com>
2020-07-07 17:12:39 +0200

    Update parametre.php

--- parametre.php
+++ parametre.php
@@ -14,18 +14,18 @@ header('location:index.php');
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="style.css" />
-    <title></title>
+    <title>Parametre</title>
   </head>
-  <header>
+    <body>
     <?php include("header.php");
-    $id = $_SESSION['id']; ?></h1>
-  </header>
+    $id = $_SESSION['id']; ?>
+
   <div class="test">
 <!-- Modification des parametres souhaites  -->
-  <body></br>
+<br/>
 <h3>Changer de nom utilisateur</h3>
 <p>
-  <form action="" method="POST" >
+  <form action="#" method="POST" >
   <label for="pseudo"></label><br/>
   <input type="text" name="pseudo" id="pseudo" /> <br/> <br/>
   <input type="submit" value="Changer votre pseudo" /> </form>
@@ -36,13 +36,13 @@ header('location:index.php');
       $stmt = $db->prepare("UPDATE membre_gbaf SET pseudo='$pseudo' WHERE id='$id'");
       $stmt->execute(array("UPDATE membre_gbaf SET pseudo='$pseudo' WHERE id='$id'"));
       echo $stmt->rowCount() . " records UPDATED successfully";
-  } ?></br>
-<h3>Chnager de mot de passe</h3>
+  } ?><br/>
+<h3>Changer de mot de passe</h3>
 <p>
-  <form action="" method="POST">
-  <label for="pass"></label><br>
-  <input type="password" name="pass" id="pass" /></br></br>
-  <input type="submit" value="Changer votre mot de passe" /></form>
+  <form action="#" method="POST">
+  <label for="pass"/> <br/>
+  <input type="password" name="pass" id="pass" /><br/><br/>
+  <input type="submit" id="pass" value="Changer votre mot de passe" /> </form>
   <?php
     if (isset($_POST['pass'])) {
         $pass = addslashes(htmlspecialchars(htmlentities(trim($_POST['pass']))));
@@ -50,12 +50,12 @@ header('location:index.php');
         $stmt = $db->prepare("UPDATE membre_gbaf SET pass='$pass_hash' WHERE id='$id'");
         $stmt->execute(array("UPDATE membre_gbaf SET pass='$pass_hash' WHERE id='$id'"));
     } ?>
-  </p></p></br>
+  </p><br/>
 <h3>Changer de nom</h3>
 <p>
   <form action="" method="POST" >
-  <label for="nom"></label><br>
-  <input type="text" name="nom" id="nom" /></br></br>
+  <label for="nom"></label><br/>
+  <input type="text" name="nom" id="nom" /><br/><br/>
   <input type="submit" value="Changer votre nom" /></form><br/>
   <?php
     if (isset($_POST['nom'])) {
@@ -68,8 +68,8 @@ header('location:index.php');
 <p>
   <form action="" method="POST">
   <label for="prenom"></label><br/>
-  <input type="text" name="prenom" id="prenom" /><br/></br>
-  <input type="submit" value="Changer votre prenom" /><br/></form></br>
+  <input type="text" name="prenom" id="prenom" /><br/><br/>
+  <input type="submit" value="Changer votre prenom" /><br/></form><br/>
   <?php
     if (isset($_POST['prenom'])) {
         $prenom = addslashes(htmlspecialchars(htmlentities(trim($_POST['prenom']))));;
@@ -80,14 +80,14 @@ header('location:index.php');
 <h3>Changer de question et de reponse</h3>
 <p>
   <form action="" method="POST">
-  <label for="question"></label><br>
-  <select id="question" name="question"></br>
+  <label for="question"></><br/>
+  <select id="question" name="question"><br/>
   <option value="Votre premiere voiture ?">Votre premiere voiture ?</option>
   <option value="Le nom de jeune fille de votre mère ?">Le nom de jeune fille de votre mère ?</option>
   <option value="Le nom de votre premier animal de compagnie ?">Le nom de votre premier animal de compagnie ? </option>
   <option value="Dans quelle ville êtes vous née ?">Dans quelle ville êtes vous née ?</option>
-  <label for="pass_question">Taper votre nouvelle reponse a la question :</label><br>
-  <input type="password" name="pass_question" id="pass_question" /></br></br>
+  <label for="pass_question">Taper votre nouvelle reponse a la question :</label><br/>
+  <input type="password" name="pass_question" id="pass_question" /><br/><br/>
   <input type="submit" value="Changer question & reponse" /><br/></form><br/>
   <?php
     if (isset($_POST['question']) && isset($_POST['pass_question'])) {
@@ -98,6 +98,6 @@ header('location:index.php');
         $stmt->execute(array("UPDATE membre_gbaf SET question='$question', pass_question='$pass_hash'  WHERE id='$id'"));
     } ?>
   </p>
-<footer><?php    include("footer.php"); ?></footer>
-</body></div>
-</html> 
+<?php    include("footer.php"); ?>
+</div></body>
+</html>
