fec1871928ed3db518d1257b279a7b818b603df5
AColardelle <67693923+AColardelle@users.noreply.github.com>
2020-07-07 17:11:23 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,22 +1,14 @@
-<!DOCTYPE html>
-<html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css">
-    <title></title>
-  </head>
+
 <footer>
 
-  <br>
+  <br/>
   <div class="lien_footer">
 
 
---<a href="#">Mention legal</a> -
+--<a href="mention_legal.php">Mention legal</a> -
 <a href="#">Contact</a>--
   </div>
  <p> Copyright (c) 2020 Copyright Holder All Rights Reserved.</p>
 
- <br>
+ <br/>
 </footer>
-</html>
- 
