823a3eaa40f47d23bc04128ac7b12faaef88a8ce
AColardelle <67693923+AColardelle@users.noreply.github.com>
2020-07-10 09:58:12 +0200

    Update nombre_com.php

--- includes/nombre_com.php
+++ includes/nombre_com.php
@@ -1,8 +1,6 @@
 <?php /*on regarde le nombre de commentaire par rapport a l'article*/
-include('connexion_db.php');
-$commentaire = $db -> prepare('SELECT *  FROM post WHERE id_acteur = ? ');
-$commentaire -> execute(array($id));
-$commentaire = $commentaire -> rowCount();
-
-
- ?> 
+ include('connexion_db.php');
+ $commentaire = $db -> prepare('SELECT *  FROM post WHERE id_acteur = ? ');
+ $commentaire -> execute(array($id));
+ $commentaire = $commentaire -> rowCount();
+?> 
