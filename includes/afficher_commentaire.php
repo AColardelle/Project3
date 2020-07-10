61476f97a18da0ee4206bd53266aa05dcad07daa
AColardelle <67693923+AColardelle@users.noreply.github.com>
2020-07-10 09:56:39 +0200

    Update afficher_commentaire.php

--- includes/afficher_commentaire.php
+++ includes/afficher_commentaire.php
@@ -1,20 +1,16 @@
 <?php /* On recupere les commentaires dans la base de donnes pour les affiches */
- $req = $db->prepare('SELECT id_user, post, DATE_FORMAT(date_add, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr FROM post WHERE id_acteur = ? ORDER BY date_add DESC');
-$req->execute(array($_GET['id_acteur']));
+  $req = $db->prepare('SELECT id_user, post, DATE_FORMAT(date_add, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr FROM post WHERE id_acteur = ? ORDER BY date_add DESC');
+  $req->execute(array($_GET['id_acteur']));
+  $test = $db -> prepare('SELECT pseudo FROM membre_gbaf where id = ?');
+  $test-> execute(array($_GET['id_acteur']));
 
-$test = $db -> prepare('SELECT pseudo FROM membre_gbaf where id = ?');
-$test-> execute(array($_GET['id_acteur']));
-
-
-while ($donnees = $req->fetch()) {
+  while ($donnees = $req->fetch()) {
     $test = $db -> prepare('SELECT pseudo FROM membre_gbaf where id = ?');
     $test-> execute(array($donnees['id_user']));
-
     $info_user = $test->fetch(); ?>
-<div class="comment"> <p><strong><?php echo htmlspecialchars($info_user['pseudo']); ?></strong> le <?php echo $donnees['date_commentaire_fr']; ?></p>
-<p><?php echo nl2br(htmlspecialchars($donnees['post'])); ?></p> </div>
-<?php
-}
 
- ?>
- 
+    <div class="comment">
+      <p> <strong><?php echo htmlspecialchars($info_user['pseudo']); ?></strong><br/> le <?php echo $donnees['date_commentaire_fr']; ?></p><br/>
+      <p><?php echo nl2br(htmlspecialchars($donnees['post'])); ?></p>
+    </div>
+<?php}?>
