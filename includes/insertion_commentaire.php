5334fa7a0efc79a5b7360c059a09173b2410b5df
AColardelle <67693923+AColardelle@users.noreply.github.com>
2020-07-10 09:57:51 +0200

    Update insertion_commentaire.php

--- includes/insertion_commentaire.php
+++ includes/insertion_commentaire.php
@@ -1,13 +1,10 @@
 <?php /*insertion des commentaires dans la base de donnes*/
 
-include("connexion_db.php");
-
-if (isset($_POST['id_user']))
-{
-  $req = $db->prepare('INSERT INTO post (id_user, id_acteur, date_add, post) VALUES (?,?, now(), ?)');
-$req -> execute(array($_POST['id_user'], $_POST['id_acteur'], $_POST['post']));
-}
-else {
-
-}
-?> 
+  include("connexion_db.php");
+  if (isset($_POST['id_user']))
+  {
+    $req = $db->prepare('INSERT INTO post (id_user, id_acteur, date_add, post) VALUES (?,?, now(), ?)');
+    $req -> execute(array($_POST['id_user'], $_POST['id_acteur'], $_POST['post']));
+  }
+  else {}
+?>
