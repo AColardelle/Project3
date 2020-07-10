54f575a4387ce434b7beabef22bd1bb96f1a0225
AColardelle <67693923+AColardelle@users.noreply.github.com>
2020-07-10 09:58:37 +0200

    Update nombre_like.php

--- includes/nombre_like.php
+++ includes/nombre_like.php
@@ -1,12 +1,10 @@
-<?php /*on compt le nombre de like ou dislike par article*/
-include('connexion_db.php');
-$likes = $db -> prepare('SELECT vote  FROM vote WHERE id_acteur = ? AND vote = ?');
-$likes -> execute(array($id, '1'));
-$likes = $likes -> rowCount();
+<?php /*on compte le nombre de like ou dislike par article*/
+  include('connexion_db.php');
+  $likes = $db -> prepare('SELECT vote  FROM vote WHERE id_acteur = ? AND vote = ?');
+  $likes -> execute(array($id, '1'));
+  $likes = $likes -> rowCount();
 
-$dislike = $db -> prepare('SELECT vote FROM vote WHERE id_acteur = ? AND vote =?');
-$dislike -> execute(array($id, '-1'));
-$dislike = $dislike -> rowCount();
-
- ?>
- 
+  $dislike = $db -> prepare('SELECT vote FROM vote WHERE id_acteur = ? AND vote =?');
+  $dislike -> execute(array($id, '-1'));
+  $dislike = $dislike -> rowCount();
+?>
