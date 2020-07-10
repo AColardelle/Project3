c77c44c0284e5903f72475fe9f4cb5bbb071052a
AColardelle <67693923+AColardelle@users.noreply.github.com>
2020-07-10 09:56:14 +0200

    Update connexion_db.php

--- connexion bdd/connexion_db.php
+++ connexion bdd/connexion_db.php
@@ -1,17 +1,16 @@
 <?php  /*Connexion a la base de donnee*/
 
-$servername = "";
-$username = "";
-$password = "";
-$database = "";
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "gbaf";
 
-try
-{
-  $db = new PDO("mysql:host=$servername;dbname=$database", $username, $password);  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
-
+  try
+  {
+    $db = new PDO("mysql:host=$servername;dbname=$database", $username, $password);  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+  catch(Exception $e)
+  {
+    die('Erreur : '.$e->getMessage());
+  }
 ?>
